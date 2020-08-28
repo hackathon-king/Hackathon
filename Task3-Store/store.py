3eca52796be70c1cabaa253bd7159ced22269292
Tian Jihui <***>
2020-08-28 11:13:13 +0800

    Backup

---
+++ Task3-Store/store.py
@@ -0,0 +1,10 @@
+"""
+2020.08.28
+3) Backup all files in /Backup and databases and Store in a remote place.
+    a. Use the python package os and re
+"""
+
+# #!/usr/bin/python3
+# coding:utf-8
+
+print("hhh")
