788ff5fa45d2e632b5505b241cbb5c014eb982ff
Tian Jihui <***>
2020-08-28 11:53:36 +0800

    'git'

--- Task3-Store/store.py
+++ Task3-Store/store.py
@@ -7,4 +7,19 @@
 # #!/usr/bin/python3
 # coding:utf-8
 
-print("hhh")
+from git import Repo
+
+
+repo = Repo(r"..")
+print(repo.active_branch)
+
+
+print(repo.tags)
+
+from git import Git
+g = Git(r"..")
+
+
+print(g.execute("git add --all"))
+print(g.execute("git commit -m 'git'"))
+print(g.execute("git log"))
